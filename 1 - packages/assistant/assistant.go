2a68d449a21e89de6b2b4411bd3c1ecb21b2f23b
Carlos Eduardo Dias Batista <carloseduardodiasbatista@gmail.com>
2022-08-20 20:24:03 -0300

    feat: aula 1

---
+++ 1 - packages/assistant/assistant.go
@@ -0,0 +1,9 @@
+package assistant
+
+import "fmt"
+
+// Register message in screen
+func Writer() {
+	fmt.Println("Escrevendo do pacote com auxiliar")
+	writer2()
+}
---
+++ 1 - packages/assistant/assistant2.go
@@ -0,0 +1,7 @@
+package assistant
+
+import "fmt"
+
+func writer2() {
+	fmt.Println("Escrevendo pela função 2")
+}
