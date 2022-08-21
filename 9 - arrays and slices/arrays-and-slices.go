28e0b7e8f4413e8608bfbed11123097c6a6d34b2
Carlos Eduardo Dias Batista <carloseduardodiasbatista@gmail.com>
2022-08-21 13:41:16 -0300

    feat: aula 9 slices

--- 9 - arrays and slices/arrays-and-slices.go
+++ 9 - arrays and slices/arrays-and-slices.go
@@ -29,4 +29,23 @@ func main() {
 
 	array2[1] = "Posição alterada"
 	fmt.Println(slice2)
+
+	// Arrays internos
+	fmt.Println("----------------------")
+	slice3 := make([]float32, 10, 11)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // tamanho
+	fmt.Println(cap(slice3)) // capacidade
+
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 6)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // tamanho
+	fmt.Println(cap(slice3)) // capacidade
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	fmt.Println(len(slice4)) // tamanho
+	fmt.Println(cap(slice4)) // capacidade
 }
